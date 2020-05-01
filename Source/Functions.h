2c89599c705eefd536db85f9367e846cd2d9b523
Bogdan Chisalescu <bogdan.chisalescu@gmail.com>
2020-05-01 19:41:28 +0300

    Started the implementation of the objective functions and encountered linking problems with Armadillo, also wrote some test code in Main.cpp

---
+++ Source/Functions.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<armadillo>
+using namespace arma;
+
+////Objective functions
+
+
+//Ellipsoidal function -- fELP
+//Schwefel function -- fSCH
+//Generalized Rosenbrock function -- fROS
+//Ackley function -- fACKL
+//Rastrigin function -- fRTG
+//Rotated Rastrigin function -- fRRTG
+
+double fELP(vec x) { //not the function yet
+
+	return dot(x, x);
+}
+
+
+
+
+
+
+
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,11 +1,24 @@
 #include<iostream>
+#include<armadillo>
+#include"Functions.h";
 using namespace std;
+using namespace arma;
 
 int main() {
 
 	cout << "TOP" << endl;
 	cout << "TOP" << endl;
 
+	mat A(2, 2);
+	A.fill(3.0);
+	vec x(3);
+	x[0] = 1;
+	x[1] = 2;
+	x[3] = 3;
+	cout << size(A) << endl;
+	cout << A.size() << endl;
+	cout << dot(x,x);
 
+	cin.get();
 	return 0;
 }
